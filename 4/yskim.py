7fd1e255972229da32a79a7bb9b599d92953b2e6
yskim <68675162+yskim1014@users.noreply.github.com>
2021-06-29 10:53:44 +0900

    Update yskim.py

--- 4/yskim.py
+++ 4/yskim.py
@@ -2,13 +2,10 @@ import sys
 from itertools import combinations
 n=int(sys.stdin.readline())
 ability_matrix = tuple(list(map(int, input().split())) for _ in range(n))
-for i in range(n):
-    ability_matrix[i][i]=0
 combi = combinations([i for i in range(n)], n//2)
 result=100*20
-for i in combi:
-    half_matrix=(ability_matrix[j][k] for k in i for j in i)
-    half_matrix_other=(ability_matrix[n-1-j][n-1-k] for k in i for j in i)
-    result = min(abs(sum(half_matrix)-sum(half_matrix_other)), result)
+for c in range(len(combi)//2):
+    team_a=sum(ability_matrix[i][j] for i in combi[c] for j in combi[c])
+    team_b=sum(ability_matrix[i][j] for i in combi[len(combi)-c-1] for j in combi[len(combi)-c-1])
+    result = min(abs(team_a-team_b),result)
 print(result)
-#근데 틀렸음...
