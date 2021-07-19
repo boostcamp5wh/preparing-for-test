a5b9d3588d7d856f7c885c9540e9b0a84078b966
yskim <68675162+yskim1014@users.noreply.github.com>
2021-07-19 15:43:18 +0900

    Update yskim.py

--- solution/29/yskim.py
+++ solution/29/yskim.py
@@ -6,11 +6,9 @@ def solution(places, answer=[]):
             for j in range(5):
                 if (abs(2-i)+abs(2-j)<=2) and (0<=r-2+i<5) and (0<=c-2+j<5):
                     mat[i][j] = mp[matrix[r-2+i][c-2+j]]
-        if mat[1][2]==1 or mat[2][1]==1 or mat[2][3]==1 or mat[3][2]==1:
-            return 1
-        if (mat[0][2]+mat[1][2])>0 or (mat[2][0]+mat[2][1])>0 or (mat[2][4]+mat[2][3])>0 or (mat[4][2]+mat[3][2])>0:
-            return 1
-        if (mat[1][1]==1 and mat[1][1]+mat[1][2]+mat[2][1]>-1) or (mat[1][3]==1 and  mat[1][3]+mat[1][2]+mat[2][3]>-1) or \
+        if mat[1][2]==1 or mat[2][1]==1 or mat[2][3]==1 or mat[3][2]==1 or \
+        (mat[0][2]+mat[1][2])>0 or (mat[2][0]+mat[2][1])>0 or (mat[2][4]+mat[2][3])>0 or (mat[4][2]+mat[3][2])>0 or \
+        (mat[1][1]==1 and mat[1][1]+mat[1][2]+mat[2][1]>-1) or (mat[1][3]==1 and  mat[1][3]+mat[1][2]+mat[2][3]>-1) or \
         (mat[3][1]==1 and  mat[3][1]+mat[2][1]+mat[3][2]>-1) or (mat[3][3]==1 and  mat[3][3]+mat[2][3]+mat[3][2]>-1):
             return 1
         return 0
